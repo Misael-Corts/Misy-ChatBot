786fbc5080ec66a75060b501dd24e2a1a380124f
Misael-Cortes <misael.cortes39@gmail.com>
2023-11-04 04:23:22 -0300

    contexto del chat con misy

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -2,43 +2,63 @@ import { NextResponse } from "next/server" //next respose nos permite enviar una
 import { Configuration, OpenAIApi } from "openai"
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 })
 
 if (!configuration.apiKey)
-throw new Error('OPENAI_API_KEY no esta definida')
+  throw new Error('OPENAI_API_KEY no esta definida')
 
 const openia = new OpenAIApi(configuration) //con esta variable podemos utilizar los multiples modelos que nos proporciona openia
 
-export async function POST (request) {
+export async function POST(request) {
 
-    const body = await request.json() //metodo del post para extraer el valor
-    const misy = 'Te llamas "Misy" eres un chatbot de acompañamiento emocional muy amigable'
-    // que apoya a las personas que padecen ansiedad o depresión y ayuda a que no se sientan solos'
-    if (!body.prompt || body.prompt.length === 0) {
-        return NextResponse.error(new Error("Debes escribir un prompt"), {
-          status: 400,
-        });
-      }
+  const body = await request.json() //metodo del post para extraer el valor
+  const misy = 'Te llamas "Misy" eres un chatbot de acompañamiento emocional muy amigable'
+  if (!body.prompt || body.prompt.length === 0) {
+    return NextResponse.error(new Error("Debes escribir un prompt"), {
+      status: 400,
+    });
+  }
+  console.log('aqui el body => ', body);
 
-      try {
-        const response = await openia.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "system", content: misy},
-                     {role: "user", content: body.prompt},
-                     {role: "assistant", content: 'Hola, Me llamo Misy'},
-                     {role: "assistant", content: 'Espero que hoy te sientas muy bien'}
+  const msj = [
+    { role: "assistant", content: "Estoy aquí para escucharte y apoyarte." },
+    { role: "assistant", content: "¿Cómo te sientes en este momento?" },
+    //empatia
+    { role: "assistant", content: "Entiendo que puedas estar pasando por un momento difícil. Estoy aquí para acompañarte." },
+    //preguntas abiertas
+    { role: "assistant", content: "¿Puedes contarme más sobre lo que estás experimentando?" },
+    //sentimientos
+    { role: "assistant", content: "Es normal sentirse así a veces. Estoy aquí para escucharte y brindarte apoyo." },
+    //sugerir acciones positivas
+    { role: "assistant", content: "Una caminata corta al aire libre a menudo puede ayudar a aliviar el estrés. ¿Te gustaría intentarlo?" },
+    //recordatorios de cuidado personal
+    { role: "assistant", content: "Recuerda cuidarte a ti mismo. Practicar la autocompasión es fundamental para tu bienestar emocional." },
+    // reafirmar disponibilidad
+    { role: "assistant", content: "No dudes en escribirme en cualquier momento. Estoy aquí para ti." },
+  ]
 
-        ],
-          temperature: 0.7,
-          max_tokens: 60,
-          top_p: 1,
-          frequency_penalty: 1,
-          presence_penalty: 1,
-        });
-    
-        return NextResponse.json(response.data.choices[0].message.content);
-      } catch (error) {
-        return NextResponse.error(error, { status: 500 });
-      }
+  // Construir los mensajes
+  const messages = [{ role: "system", content: misy }];
+  if (body.chatLog) {
+    messages.push(...body.chatLog, ...msj); // Agregar mensajes del historial
+  }
+  messages.push({ role: "user", content: body.prompt });
+
+  try {
+    console.log('aqui body.chatlog =>', ...body.chatLog);
+    const response = await openia.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0.3,
+      max_tokens: 256,
+      // top_p: 1,
+      // frequency_penalty: 1,
+      // presence_penalty: 1,
+    });
+    console.log('mensajesss back', response.data.choices[0].message.content);
+    return NextResponse.json(response.data.choices[0].message.content);
+  } catch (error) {
+    return NextResponse.error(error, { status: 500 });
+  }
 }
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,126 +7,109 @@ import user from "../../public/img/userInvitado.png";
 import logoMsg from "../../public/img/MISI.png";
 import TypingAnimation from "./TypingAnimation";
 
-export default function Chat () {
-    const [prompt, setPrompt] = useState ("") //lo que el usuario escribe o pregunta
-    const [result, setResult] = useState();
-    const [chatLog, setChatLog] = useState([])
-    const [loading, setLoading] = useState(false);
-    
-  
-    const generateMessage = async (prompt) => {
-      setLoading(true)
-      try {
-        const response = await fetch('/api/generate', {
+export default function Chat() {
+  const [prompt, setPrompt] = useState("") //lo que el usuario escribe o pregunta
+  const [result, setResult] = useState();
+  const [chatLog, setChatLog] = useState([])
+  const [loading, setLoading] = useState(false);
+
+
+  const generateMessage = async (prompt) => {
+    setLoading(true)
+    try {
+      const response = await fetch('/api/generate', {
         method: 'POST',
         headers: {           //danmas info de lo que estamos pidiendo
-          'Content-Type': 'application/json'       
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({prompt}) //convertir objeto con propiedad prompt 
+        body: JSON.stringify({ prompt, chatLog }) //convertir objeto con propiedad prompt 
       })
-  
       const data = await response.json() // convertir la respuesta a un json
       setResult(data);
-      setChatLog((prevChatLog) => [...prevChatLog, { type: 'bot', message: data }])
-
-  
+      setChatLog((prevChatLog) => [...prevChatLog, { role: "assistant", content: data }])
     } catch (error) {
       alert(error.message);
       return;
     }
-  
-   setLoading(false)
-  
-    }
-  
+    setLoading(false)
+  }
+
   const onSubmit = async (e) => { //No se reinicie la pagina cada vez que se envie el formulario
-      e.preventDefault()
-      setChatLog((prevChatLog) => [...prevChatLog, { type: 'user', message: prompt }]);
-      generateMessage(prompt);
-      setPrompt('');
-    }
+    e.preventDefault()
+    setChatLog((prevChatLog) => [...prevChatLog, { role: 'user', content: prompt }]);
+    generateMessage(prompt);
+    setPrompt('');
+  }
 
-    console.log('aquie chatlog => ',chatLog);
+  console.log('aquie chatlog => ', chatLog);
 
- return (
- <div className={styles.chatContainer}>
-
-  <Image width={300}
-          src={Logo}
-        />
-
-
-   
-  <div className={styles.prueba}>
-
-    <div className={styles.chatMessages}>
- 
-      {result && (   
-           <div>
-           {
-            chatLog.map((message, index) => (
-        <div key={index} className={`flex ${
-          message.type === 'bot' ? 'flex-row items-center mt-3 mb-3' : 'flex-row-reverse items-center mt-3'
-          }`}>
-                <div className={`${message.type === 'bot' ? 'rounded-full overflow-hidden mr-2' : 'rounded-full overflow-hidden ml-2'}`}>
-                  <Image
-                    width={50}
-                    height={50}
-                    src={message.type === 'bot' ? logoMsg : user}
-                    alt="Misy"
-                    />
-                </div>
-                <p className={styles.botText}>
-                {/* {loading && message.type === 'bot' ? '...' : message.message} */}
-                {message.message}
-                </p>
-              </div> 
-            ))
-           }
-                {
-              loading &&
-              <div key={chatLog.length} className="flex justify-start">
+  return (
+    <div className={styles.chatContainer}>
+      <Image width={300}
+        src={Logo}
+        alt="Misy"
+      />
+      <div className={styles.prueba}>
+        <div className={styles.chatMessages}>
+          {result && (
+            <div>
+              {
+                chatLog.map((message, index) => (
+                  <div key={index} className={`flex ${message.role === 'assistant' ? 'flex-row items-center mt-3 mb-3' : 'flex-row-reverse items-center mt-3'
+                    }`}>
+                    <div className={`${message.role === 'assistant' ? 'rounded-full overflow-hidden mr-2' : 'rounded-full overflow-hidden ml-2'}`}>
+                      <Image
+                        width={50}
+                        height={50}
+                        src={message.role === 'assistant' ? logoMsg : user}
+                        alt="Misy"
+                      />
+                    </div>
+                    <p className={styles.botText}>
+                      {/* {loading && message.type === 'bot' ? '...' : message.message} */}
+                      {message.content}
+                    </p>
+                  </div>
+                ))
+              }
+              {
+                loading &&
+                <div key={chatLog.length} className="flex justify-start">
                   <div className={'rounded-full overflow-hidden mr-2'}>
-                  <Image
-                    width={50}
-                    height={50}
-                    src={logoMsg}
-                    alt="Misy"
+                    <Image
+                      width={50}
+                      height={50}
+                      src={logoMsg}
+                      alt="Misy"
                     />
-                </div>
+                  </div>
                   <div className={styles.botText}>
                     <TypingAnimation />
                   </div>
-              </div>
-            }
-       </div> 
-      )}
-    </div>
-    
- <form onSubmit={onSubmit} className={styles.formContainer} >      
-   <div className={styles.chatUser}>
+                </div>
+              }
+            </div>
+          )}
+        </div>
+        <form onSubmit={onSubmit} className={styles.formContainer} >
+          <div className={styles.chatUser}>
 
-      <input
-          type="text"
-          name="mensaje"
-          placeholder="Escriba su mensaje"
-          onChange={(e) => setPrompt(e.target.value)}
-          className={styles.input}
-          value={prompt}
-          autoFocus
-        />
-          
-        <button type="submit" className={styles.sendButton} disabled={!prompt || loading}>
-          Enviar
-        </button>
-   </div>
-   </form>
+            <input
+              type="text"
+              name="mensaje"
+              placeholder="Escriba su mensaje"
+              onChange={(e) => setPrompt(e.target.value)}
+              className={styles.input}
+              value={prompt}
+              autoFocus
+            />
 
-   </div>
-
-      
-      
+            <button type="submit" className={styles.sendButton} disabled={!prompt || loading}>
+              Enviar
+            </button>
+          </div>
+        </form>
       </div>
-    
- )  
+    </div>
+  )
 }
